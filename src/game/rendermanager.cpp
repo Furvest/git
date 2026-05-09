23f929faa2cba30238ded06aef1acd41fd0582e3
shkaberined <shkaberined@mauniver.ru>
2026-05-09 19:06:41 +0300

    отрисовка шрифта

--- src/game/rendermanager.cpp
+++ src/game/rendermanager.cpp
@@ -1,19 +1,15 @@
 #include "rendermanager.hpp"
+#include "fs.hpp"
+
+Vector Renderer::GetScreenSize()
+{
+    int w, h;
+    SDL_GetRenderLogicalPresentation(r, &w, &h, nullptr);
+    return Vector((float)w,(float)h);
+}
 
 Renderer::Renderer() {};
 void Renderer::Render(SDL_Texture* tex, Vector pos, Quad crop_region, Vector pivot, Vector scale, double rotation, ColorDesc color) {
-/*	SDL_FRect src_rect{ .x = (float)crop_region.topleft.x,.y=(float)crop_region.topleft.y,.w=(float)crop_region.widthheight.x,.h=(float)crop_region.widthheight.y };
-	Vector crop_rel=crop_region.widthheight;
-	SDL_FPoint render_pivot{ .x = (float)pivot.x,.y = (float)pivot.y };
-	SDL_FRect dst_rect{ .w = (float)crop_rel.x*(float)scale.x,.h = (float)crop_rel.y * (float)scale.y };
-	dst_rect.x = pos.x - pivot.x * (scale.x - 1.0);
-	dst_rect.y = pos.y - pivot.y * (scale.y - 1.0);
-	pivot.x = pivot.x * scale.x;
-	pivot.y = pivot.y * scale.y;
-	SDL_SetTextureColorModFloat(tex, (float)color.R, (float)color.G, (float)color.B);
-	SDL_SetTextureAlphaModFloat(tex, (float)color.A);
-	SDL_RenderTextureRotated(r,tex,&src_rect,&dst_rect,rotation,&render_pivot,SDL_FLIP_NONE);*/
-
     Vector final_scale{
         scale.x * globalScale,
         scale.y * globalScale
@@ -57,10 +53,25 @@ void Renderer::Render(SDL_Texture* tex, Vector pos, Quad crop_region, Vector piv
         &render_pivot,
         SDL_FLIP_NONE
     );
+}
+void Renderer::RenderFont(const std::string& text, Vector pos) //функция дорогая! надо будет придумать способ рисовать получше
+{
+    SDL_Color c;
+    c.r = 0;
+    c.g = 0;
+    c.b = 0;
+    c.a = 255;
+    TTF_Text* drawn_text=TTF_CreateText(text_engine, main_font, text.c_str(), text.size());
+    TTF_SetTextColor(drawn_text, c.r, c.g, c.b, c.a);
+    TTF_DrawRendererText(drawn_text, pos.x, pos.y);
+    TTF_DestroyText(drawn_text);
 };
 
 void Renderer::Init(SDL_Renderer* renderer) {
 	r = renderer;
+    TTF_Init();
+    text_engine=TTF_CreateRendererTextEngine(r);
+    main_font = TTF_OpenFont( (FSManager::GetAssetPath() + "font.ttf").c_str(),36.0f);
 };
 
 Renderer g_Renderer;
--- src/game/rendermanager.hpp
+++ src/game/rendermanager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <SDL3/SDL.h>
 #include "common/vector.hpp"
+#include <SDL3_ttf/SDL_ttf.h>
+#include <string>
 
 struct ColorDesc {
 	double R=1.0;
@@ -12,10 +14,14 @@ struct ColorDesc {
 class Renderer {
 	SDL_Renderer* r=nullptr;
 public:
-	float globalScale = 2.0f;
+	Vector GetScreenSize();
+	float globalScale = 1.0f;
+	TTF_Font* main_font = 0x0;
+	TTF_TextEngine* text_engine = 0x0;
 	Renderer();
 	void Init(SDL_Renderer* renderer);
 	void Render(SDL_Texture* tex, Vector pos, Quad crop_region, Vector pivot, Vector scale, double rotation,ColorDesc color);
+	void RenderFont(const std::string& text, Vector pos);
 };
 
 extern Renderer g_Renderer;
