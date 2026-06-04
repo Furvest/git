1849804e1971a942d2ba0d90722076b77074c800
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 12:48:39 +0300

    Update titlescene.hpp

--- src/game/titlescene.hpp
+++ src/game/titlescene.hpp
@@ -1,7 +1,26 @@
+/**
+ * @file titlescene.hpp
+ * @brief Заголовочный файл начального экрана
+ *
+ * Здесь объявлен класс TitleScene, который наследуется от базового Scene.
+ * Это начальный экран игры
+ * 
+ */
+
 #pragma once
 #include "scene.hpp"
 #include "anm2sprite.hpp"
 #include "manager.hpp"
+
+/**
+ * @class TitleScene
+ * @brief Класс начального экрана
+ *
+ * Поля: спрайты anm2 с заголовком, кнопкой "начать" и "выйти"
+ * Методы: обновление, отрисовка, обработка событий, закрытие сцены
+ * 
+ */
+
 class TitleScene : public Scene {
 	ANM2::Sprite title;
 	ANM2::Sprite begin_button;
