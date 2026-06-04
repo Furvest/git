41cb00855a6534f8e1f54534b5653d8b6f1e5a05
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 11:06:03 +0300

    Добавил html-документацию
    Все работает, кнопочки тыкаются, поля описываются, иерархия классов присутствует

---
+++ docs/files_dup.js
@@ -0,0 +1,19 @@
+var files_dup =
+[
+    [ "anm2data.hpp", "anm2data_8hpp_source.html", null ],
+    [ "anm2sprite.hpp", "anm2sprite_8hpp_source.html", null ],
+    [ "anm2state.hpp", "anm2state_8hpp_source.html", null ],
+    [ "audiomanager.hpp", "audiomanager_8hpp_source.html", null ],
+    [ "dialogueholder.hpp", "dialogueholder_8hpp_source.html", null ],
+    [ "dialoguescene.hpp", "dialoguescene_8hpp_source.html", null ],
+    [ "eventscene.hpp", "eventscene_8hpp_source.html", null ],
+    [ "fs.hpp", "fs_8hpp_source.html", null ],
+    [ "gamestate.hpp", "gamestate_8hpp_source.html", null ],
+    [ "inputmanager.hpp", "inputmanager_8hpp_source.html", null ],
+    [ "manager.hpp", "manager_8hpp_source.html", null ],
+    [ "rendermanager.hpp", "rendermanager_8hpp_source.html", null ],
+    [ "scene.hpp", "scene_8hpp_source.html", null ],
+    [ "scenemanager.hpp", "scenemanager_8hpp_source.html", null ],
+    [ "texmanager.hpp", "texmanager_8hpp_source.html", null ],
+    [ "titlescene.hpp", "titlescene_8hpp_source.html", null ]
+];
---
+++ docs/search/all_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['inputmanager_0',['InputManager',['../class_input_manager.html',1,'']]],
+  ['interpolated_1',['Interpolated',['../struct_a_n_m2_1_1_frame_data.html#a4713c205767b696679ecbb09dabebc5b',1,'ANM2::FrameData']]]
+];
