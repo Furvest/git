61e7638d78d3fdd8f9343d8e3a41417e9aa4ffbd
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 12:48:10 +0300

    Update texmanager.hpp

--- src/game/texmanager.hpp
+++ src/game/texmanager.hpp
@@ -1,12 +1,36 @@
+/**
+ * @file texmanager.hpp
+ * @brief Заголовочный файл менеджера текстур
+ *
+ * Здесь содержится объявление класса TextureManager, отвечающего за операции с текстурами
+ * Помимо этого, присутствует объявление структуры TexEntry, объекты которой образуют список текстур
+ */
+
 #pragma once
 #include <string>
 #include <vector>
 #include <SDL3/SDL.h>
+
+/**
+ * @class TexEntry
+ * @brief Структура вхождения тестуры
+ *
+ * Поля: путь к текстуре, ссылка на объект класса SDL_Texture и количество ссылок на текстуру
+ */
+
 struct TexEntry {
 	std::string path;
 	SDL_Texture* texture = nullptr;
 	int refcount = 0;
 };
+
+/**
+ * @class TextureManager
+ * @brief Менеджер текстур - управляет текстурами
+ *
+ * Поля: ссылка на SDL_Renderer, вектор текстур типа TexEntry
+ * Методы: инициализация, получение, очистка и вывод текстуры
+ */
 
 class TextureManager {
 	SDL_Renderer* r=nullptr;
