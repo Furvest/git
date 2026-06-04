e82ade40bdf49b6c7e4b6f26040dc501ac50d562
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 12:42:15 +0300

    Update dialogueholder.hpp
    Документирование

--- src/game/dialogueholder.hpp
+++ src/game/dialogueholder.hpp
@@ -1,5 +1,22 @@
+/**
+ * @file dialogueholder.hpp
+ * @brief Заголовочный файл обработчика диалогов
+ *
+ * Здесь содержится объявление класса TextureManager, отвечающего за обработку диалогов 
+ * главного героя с другими персонажами
+ * 
+ */
+
 #pragma once
 #include <string>
+
+ /**
+   * @class DialogueHolder
+   * @brief Обработчик диалогов
+   *
+   * Имеет 3 приватных поля: заголовок, текст и состояние (показан ли)
+   *
+   */
 
 struct DialogueHolder {
 	std::string header;
