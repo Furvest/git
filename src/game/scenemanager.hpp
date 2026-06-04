d6ea118c62e271f37e9e0c55e4cc63e2d5d93140
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 12:47:32 +0300

    Update scenemanager.hpp

--- src/game/scenemanager.hpp
+++ src/game/scenemanager.hpp
@@ -1,7 +1,26 @@
+/**
+ * @file scanemanager.hpp
+ * @brief Заголовочный файл менеджера сцен
+ *
+ * Здесь объявлен класс SceneManager, который управляет сценами
+ *
+ */
+
 #pragma once
 #include "scene.hpp"
 #include <list>
 #include <memory>
+
+/**
+ * @class SceneManager
+ * @brief класс менеджера сцен
+ *
+ * Поля: список уникальных указателей на объекты класса Scene (сцены на добавление), список сцен и 
+ * флаг, указывающий, не изменен ли порядок сцен
+ * 
+ * Методы: создание очереди сцен, обновление очереди, обновление, отрисовка и обработка событий
+ */
+
 class SceneManager {
 	std::list<std::unique_ptr<Scene>> ScenesToAdd;
 	bool orderChanged = false;
