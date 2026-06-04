69e64d8d82f64af6f0e7442ed26c8f0f5423e588
Aleksei Kazak <68787963+ryoch1@users.noreply.github.com>
2026-06-04 12:44:23 +0300

    Update gamestate.hpp

--- src/game/gamestate.hpp
+++ src/game/gamestate.hpp
@@ -1,5 +1,20 @@
+/**
+ * @file gamestate.hpp
+ * @brief Заголовочный файл состояния игры
+ *
+ */
+
 #pragma once
 #include <string>
+
+/**
+   * @class GameState
+   * @brief Структура состояния игры
+   *
+   * Единственное поле - текущая локация
+   * 
+   */
+
 struct GameState {
 	std::string cur_field="test_field";
 };
